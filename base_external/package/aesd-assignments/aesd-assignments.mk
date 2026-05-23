cb0e1f37f4f3938b38e43eba2ff4c8a95c8e3236
ibrahim.jaime <ibrahimjaime@gmail.com>
2026-05-23 11:16:18 -0300

    Complete assignment4 buildroot integration

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,11 +6,11 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
+AESD_ASSIGNMENTS_VERSION = main
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
+AESD_ASSIGNMENTS_SITE = git@github.com:ibrahimjaime/assignment3-ibrahimjaime.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
@@ -20,9 +20,24 @@ endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+	# Create config directory
+	$(INSTALL) -d $(TARGET_DIR)/etc/finder-app/conf
+
+	# Install config files
+	$(INSTALL) -m 0755 $(@D)/finder-app/conf/* \
+		$(TARGET_DIR)/etc/finder-app/conf/
+
+	# Install writer binary
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer \
+		$(TARGET_DIR)/usr/bin/
+
+	# Install finder.sh
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh \
+		$(TARGET_DIR)/usr/bin/
+
+	# Install finder-test.sh
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh \
+		$(TARGET_DIR)/usr/bin/
 endef
 
 $(eval $(generic-package))
